5ec96105fec1ebe39ec5b69689f197776fdfc28f
Hippo <badrihippo@gmail.com>
2016-03-10 17:53:41 +0530

    Update form with new datepicker widget

--- app.py
+++ app.py
@@ -8,10 +8,20 @@ if not app.config.has_key('SECRET_KEY') or app.config['SECRET_KEY'] is None:
     app.config['SECRET_KEY'] = 'secret'
 app.config['DEBUG'] = True
 
-class DateListField(wtf.Field):
-    widget = wtf.widgets.TextInput()
+class DateListWidget(wtf.widgets.TextInput):
+    def __init__(self):
+        super(DateListWidget, self).__init__()
 
-    # TODO: Make it work with/validate dates instead of just plaintext
+    def __call__(self, field, **kwargs):
+        kwargs['data-provide'] = 'datepicker'
+        kwargs['data-date-multidate'] = 'true'
+        kwargs['data-date-format'] = 'dd-mm-yyyy'
+        return super(DateListWidget, self).__call__(field, **kwargs)
+
+
+class DateListField(wtf.Field):
+    widget = DateListWidget()
+
     def _value(self):
         if self.data:
             return u', '.join(self.data)
@@ -20,7 +30,21 @@ class DateListField(wtf.Field):
 
     def process_formdata(self, valuelist):
         if valuelist:
-            self.data = [x.strip() for x in valuelist[0].split(',')]
+            data = []
+            for v in valuelist:
+                values = v.split(',')
+                for x in values:
+                    # TODO: Check for actual valid dates, not just integer triplets
+                    d = x.strip().split('-')
+                    if len(d) == 3:
+                        try:
+                            d = [int(i) for i in d]
+                        except ValueError:
+                            raise wtf.ValidationError('Invalid date: %s. Input must be a list of dates formatted dd-mm-yyyy only.' % x)
+                        data.append(d)
+                    else:
+                        raise wtf.ValidationError('Invalid date: %s. Input must be a list of dates formatted dd-mm-yyyy only.' % x)
+                self.data = data
         else:
             self.data = []
 
