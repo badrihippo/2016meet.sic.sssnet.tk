9917a1f347376189a29cc80752c355dfbbda538a
Hippo <badrihippo@gmail.com>
2016-04-19 14:59:34 +0530

    Add non-SSSian registration form

--- app.py
+++ app.py
@@ -65,6 +65,29 @@ class NameShopContactForm(Form):
     shop = wtf.StringField('Shop name', validators=[wtf.validators.Required()])
     contact = wtf.StringField('Email or Phone', validators=[wtf.validators.Required()])
 
+class NonSSSianForm(Form):
+    name = wtf.StringField('Name', validators=[wtf.validators.Required()])
+    contact = wtf.StringField('Email or Phone', validators=[wtf.validators.Required()])
+    how_find_event = wtf.SelectField('How did you hear about this event?',
+        choices = (
+          ('Heard from friend', 'I heard about it from a friend'),
+          ('Event invitation', 'I received an invitation to this event by email'),
+          ('Aliens', 'Some aliens left a message under my pillow'),
+          ('Social network', 'I found the link in my social network stream'),
+          ('Found on Internet', 'I found this website while surfing the Internet'),
+        ))
+    how_find_sss = wtf.SelectField('How did you find out about SSS?',
+        choices = (
+            ('This is first time', 'This is the first time I\'m hearing of it!'),
+            ('Heard from friend', 'A friend told me about it'),
+            ('Found on Internet', 'I found the SSS website while surfing the Internet'),
+            ('Found on Wiki', 'I found the SSS Wiki on Wikia'),
+            ('Social Page', 'I found pages about SSS on a social network'),
+        ))
+    why_interested = wtf.TextAreaField('Please say a few words about why you would like to join...',
+        validators=[wtf.validators.Required()])
+    reference_person = wtf.StringField('Name one person you know who <em>is</em> part of SSS')
+
 db = TinyDB(app.config['TINYDB_DB_PATH'])
 dv = db.table('date_votes')
 db_reg = db.table('registrations')
@@ -154,6 +177,23 @@ def register():
         db_reg.insert(u)
         return redirect(url_for('register_thanks'))
     return render_template('register.htm', form=form)
+
+@app.route('/register/non-sssians/', methods=['GET', 'POST'])
+def register_nonsssian():
+    form = NonSSSianForm()
+    if form.validate_on_submit():
+        u = {}
+        u['name'] = form.name.data
+        u['contact'] = form.contact.data
+        u['how_find_event'] = form.how_find_event.data
+        u['how_find_sss'] = form.how_find_sss.data
+        u['why_interested'] = form.why_interested.data
+        u['reference_person'] = form.reference_person.data
+        u['timestamp'] = datetime.now().isoformat()
+
+        db_reg.insert(u)
+        return redirect(url_for('register_thanks'))
+    return render_template('register_nonsssian.htm', form=form)
 
 @app.route('/register/thanks/')
 def register_thanks():
